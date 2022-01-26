08243f3879eb639d81c2324857b1cf27fc0bde20
Tatyana Bondarenko <tanya_kursavka@mail.ru>
2022-01-26 06:47:58 +0300

    first commit

---
+++ src/main/java/ru/netology/sqr/StatsService.java
@@ -0,0 +1,85 @@
+package ru.netology.sqr;
+
+public class StatsService {
+
+
+    public long sumSales(long[] sales) {
+        long sumSales = 0;
+        for (long sale : sales) {
+            sumSales = sumSales + sale;
+        }
+        return sumSales;
+    }
+
+    public long averageSales(long[] sales) {
+        long sumSales = 0;
+        int month = 0;
+        long averageSales = 0;
+        for (long sale : sales) {
+            sumSales = sumSales + sale;
+            month = month + 1;
+            averageSales = sumSales / month;
+        }
+        return averageSales;
+    }
+
+    public int maxSales(long[] sales) {
+        int maxMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1;
+        }
+        return maxMonth + 1;
+    }
+
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1;
+        }
+        return minMonth + 1;
+    }
+
+    public int numberMonthsUnderAverage(long[] sales) {
+
+        int month = 0;
+        StatsService service = new StatsService();
+        long averageSales = service.averageSales(sales);
+        int countMonths = 0;
+        for (long sale : sales) {
+            month = month + 1;
+
+            if (sale < averageSales) {
+                countMonths++;
+            }
+        }
+        return countMonths;
+    }
+
+    public int numberMonthsOverAverage(long[] sales) {
+        int month = 0;
+        StatsService service = new StatsService();
+        long averageSales = service.averageSales(sales);
+        int countMonths = 0;
+        for (long sale : sales) {
+            month = month + 1;
+
+            if (sale > averageSales) {
+                countMonths++;
+            }
+        }
+        return countMonths;
+    }
+
+}
+
+
+
+
---
+++ src/test/java/ru/netology/sqr/StatsServiceTest.java
@@ -0,0 +1,78 @@
+package ru.netology.sqr;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StatsServiceTest {
+
+    @Test
+    void shouldCalculateSumSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 180;
+
+        long actual = service.sumSales(sales);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalculateAverageSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        long expected = 15;
+
+        long actual = service.averageSales(sales);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalculateMaxSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 8; //последний месяц, соответствующий условиям
+
+        int actual = service.maxSales(sales);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalculateMinSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expected = 9; //последний месяц, соответствующий условиям
+        int actual = service.minSales(sales);
+
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void shouldCalculateNumberMonthsUnderAverage() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expected = 5;
+        int actual = service.numberMonthsUnderAverage(sales);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldCalculateNumberOverAverage() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int expected = 5;
+
+        int actual = service.numberMonthsOverAverage(sales);
+
+        assertEquals(expected, actual);
+    }
+}
